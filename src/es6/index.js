75ceb33a35228edf175b52e7fc1dca15f312b07b
JosephMMS <josephzx1@hotmail.com>
2021-01-14 20:48:57 -0600

    LET y CONST, Multilínea, Spread Operator y Desestructuración

--- src/es6/index.js
+++ src/es6/index.js
@@ -19,8 +19,8 @@
     console.log(name,age,country);
      
  }
- newFunction2();
- newFunction2('Joseph',18);
+//  newFunction2();
+//  newFunction2('Joseph',18);
 
  /**
   * Template literals
@@ -32,5 +32,61 @@ let epicPhrase=hello+' '+world;
  * ES6
  */
 let epicPhrase2=`${hello} ${world}`;
-console.log(epicPhrase);  
-console.log(epicPhrase2);  
+// console.log(epicPhrase);  
+// console.log(epicPhrase2);  
+
+/**
+ * Multilínea antigua:
+ * Con \n + concatenabamos las lineas
+ */
+let lorem="lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lobortis porttitor \n"
++ "sagittis dolor ac mauris placerat tristique. Nam interdum ultrices hendrerit. Aliquam consequat porta ul"
+/**
+ * ES6
+ */
+let lorem2=`LOREM2 ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lobortis porttitor 
+sagittis dolor ac mauris placerat tristique. Nam interdum ultrices hendrerit. Aliquam consequat porta ul`
+//  console.log(lorem);
+//  console.log(lorem2);
+
+ /**
+  *  Desestructuración antigua
+  */
+ let person={
+     'name':'Oscar',
+     'age':32,
+     'country':'Costa Rica'
+ }
+//  console.log(person.name,person.age,person.country);
+ /**
+  * ES6
+  */
+ let {name,age,country}=person;
+//  console.log(name,age,country);
+
+/**
+ * Spread Operator ES6
+ */
+let team=['Oscar','Julian','Ricardo'];
+let team2=['Valeria','Jessica','Camila'];
+let education=['David',...team,...team2];
+// console.table(education);
+
+/**
+ * let ES6 :unicamnete esta disponible en el bloque de codigo donde este llamado(scope).
+ * Es mejor empezar a trabajar con let
+ *  */
+{
+    var global_var='global var';
+}
+{
+    let global_let='global let';
+    console.log(global_let);//let si es accecible
+}
+console.log(global_var);
+// console.log(global_let);//let no es accecible
+/**
+ * const: este tipo de variale no se puede cambiar de ninguna manera
+ */
+const a='a';
+// a='b'; Esto no lo va a permitir
